6e8f41c29f919869ac10dc1ba1c0936177154623
steefanyalvees <stefanyanjoos@hotmail.com>
2026-03-21 13:32:04 +0000

    create task entity

--- src/main/java/com/todo_list_api/entity/Todo.java
+++ src/main/java/com/todo_list_api/entity/Todo.java
@@ -1,4 +1,55 @@
 package com.todo_list_api.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean performed;
+    private int priorities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPerformed() {
+        return performed;
+    }
+
+    public void setPerformed(boolean performed) {
+        this.performed = performed;
+    }
+
+    public int getPriorities() {
+        return priorities;
+    }
+
+    public void setPriorities(int priorities) {
+        this.priorities = priorities;
+    }
 }
