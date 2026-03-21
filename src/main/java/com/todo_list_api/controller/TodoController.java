97c0e545fb2062f30abe4b5d16f96dfc674a96e9
steefanyalvees <stefanyanjoos@hotmail.com>
2026-03-21 14:17:01 +0000

    create task controller

--- src/main/java/com/todo_list_api/controller/TodoController.java
+++ src/main/java/com/todo_list_api/controller/TodoController.java
@@ -1,4 +1,38 @@
 package com.todo_list_api.controller;
 
+import com.todo_list_api.entity.Todo;
+import com.todo_list_api.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todo")
 public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+
+
 }
